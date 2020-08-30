a020daefb50828690e6bcb8b6d9602c149052888
GooseMagnet <ilankarasik@gmail.com>
2020-08-30 14:32:15 -0400

    Updates

---
+++ precache-manifest.23505519e050a7e70d5de59ebbdd0653.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f81283becb02ffbfc53ebc0bd17fef4",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "e14b613564f689983f2c",
+    "url": "/website/static/js/2.7c876dae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/website/static/js/2.7c876dae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f5f01d8de874e2d6a44",
+    "url": "/website/static/js/main.deeb3568.chunk.js"
+  },
+  {
+    "revision": "dab99bf7f662fa61e923",
+    "url": "/website/static/js/runtime-main.c569aef9.js"
+  }
+]);
